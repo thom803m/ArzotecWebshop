a5333403fc9b268c4f15a40bc2d46b4aabdf611f
thoma <thoma@THOMAS-ARZOTEC->
2026-03-19 09:41:26 +0100

    Deleted test query

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿INSERT INTO Brands (Name)
-VALUES ('Dell');
-
-INSERT INTO Categories (Name)
-VALUES ('Laptops');
-
-INSERT INTO Products (Sku, Name, Price, StockQuantity, BrandId, CategoryId, LastSynced)
-VALUES ('ABC123', 'Test Laptop', 9999, 5, 1, 1, GETDATE());
